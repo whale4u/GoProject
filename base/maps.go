d2d7da9dd505b29e97ee243c5c7082500c178628
whale4u <whale4u@hotmail.com>
2021-02-25 16:56:55 +0800

    新增map例子

---
+++ base/maps.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	//创建名为boySalary的map，其中键是 string 类型，而值是 int 类型。
+	boySalary := make(map[string]int)
+	boySalary["bob"] = 1200
+
+	//另一种创建方式
+	girlSalary := map[string]int{
+		"alice": 1300,
+		//最后一个元素也要有逗号
+		"susan": 1400,
+	}
+
+	fmt.Printf("map girlsalary's length is: %d\n", len(girlSalary))
+
+	//遍历maps
+	for key, value := range girlSalary {
+		fmt.Printf("girl[%s] = %d\n", key, value)
+	}
+
+	//删除key
+	fmt.Println("map before deletion", girlSalary)
+	delete(girlSalary, "susan")
+	fmt.Println("map after deletion", girlSalary)
+}
