bfd0649e4bc7a1c9dca6087985090e2e5086a2dd
whale4u <whale4u@hotmail.com>
2021-03-15 22:33:50 +0800

    新增commandi部分代码

---
+++ sec/commandi/commandi_fix1.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"net/http"
+	"os/exec"
+)
+
+//校验IP格式
+func IsIpv4Net(host string) bool {
+	return net.ParseIP(host) != nil
+}
+
+func commandInjectionPrepareHandler(w http.ResponseWriter, r *http.Request) {
+	// vars is map type
+	vars := r.URL.Query()
+	tmpKey, ok := vars["ip"]
+	if !ok {
+		fmt.Println(w, "ip is empty")
+		return
+	}
+	ip := tmpKey[0]
+
+	if IsIpv4Net(ip) {
+		cmdStr := "ping -c 3 " + ip
+		cmd := exec.Command("bash", "-c", cmdStr)
+		out, err := cmd.CombinedOutput()
+		if err != nil {
+			fmt.Fprint(w, err)
+		}
+		fmt.Fprint(w, string(out))
+	} else {
+		fmt.Println("IP格式错误！")
+
+	}
+
+}
+
+func main() {
+	http.HandleFunc("/command", commandInjectionPrepareHandler)
+	http.ListenAndServe(":8080", nil)
+}
