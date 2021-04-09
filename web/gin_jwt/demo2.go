8f56ae1aca82aa094166a5c3b7e3a2844320c1cd
whale4u <whale4u@hotmail.com>
2021-04-09 12:44:59 +0800

    更新为lwz的demo代码

--- web/gin_jwt/demo2.go
+++ web/gin_jwt/demo2.go
@@ -1,14 +1,27 @@
 package main
 
 import (
-	"fmt"
+	"errors"
 	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
-	"log"
 	"net/http"
+	"strings"
 	"time"
 )
+
+const TokenExpireDuration = time.Hour * 2
+
+var MySecret = []byte("夏天夏天悄悄过去")
+
+// MyClaims 自定义声明结构体并内嵌jwt.StandardClaims
+// jwt包自带的jwt.StandardClaims只包含了官方字段
+// 我们这里需要额外记录一个username字段，所以要自定义结构体
+// 如果想要保存更多信息，都可以添加到这个结构体中
+type MyClaims struct {
+	Username string `json:"username"`
+	jwt.StandardClaims
+}
 
 type User struct {
 	UserName string `json:"username"`
@@ -16,88 +29,128 @@ type User struct {
 }
 
 //curl请求：必须是json请求
-//curl --location --request POST 'http://127.0.0.1:8088/login' \
+//curl --location --request POST 'http://127.0.0.1:8088/auth' \
 //--header 'Content-Type: application/json' \
 //--data-raw '{"username":"admin", "password":"pass"}'
 
+//curl --location --request GET 'http://127.0.0.1:8088/home' \
+//--header 'Authorization:Bearer xxx'
+
 func main() {
 	r := gin.Default()
-	r.POST("/login", Login)
-	//r.GET("/logout", getting)
+	r.POST("/auth", authHandler)
+	r.GET("/home", JWTAuthMiddleware(), homeHandler)
+
 	//监听端口默认为8080
 	r.Run(":8088")
 }
 
-func Login(c *gin.Context) {
-	var u User
-	if err := c.ShouldBindJSON(&u); err != nil {
-		c.JSON(http.StatusUnprocessableEntity, "Invalid json provided")
-		fmt.Println(err)
-		return
-	}
-	//find user with username
-	//user, err := models.UserRepo.FindByID(1)
-	//compare the user from the request, with the one we defined:
-	if u.UserName != "admin" || u.Password != "pass" {
-		c.JSON(http.StatusUnauthorized, "Please provide valid login details")
-		return
-	}
-	token := CreateToken(u.UserName, u.Password)
-	//if err != nil {
-	//	c.JSON(http.StatusUnprocessableEntity, err.Error())
-	//	return
-	//}
-
-	// save token to redis
-	//saveErr := servers.HttpServer.RD.CreateAuth(user.ID, ts)
-	//if saveErr != nil {
-	//	c.JSON(http.StatusUnprocessableEntity, saveErr.Error())
-	//}
-
-	c.JSON(http.StatusOK, token)
+func homeHandler(c *gin.Context) {
+	username := c.MustGet("username").(string)
+	c.JSON(http.StatusOK, gin.H{
+		"code": 2000,
+		"msg":  "success",
+		"data": gin.H{"username": username},
+	})
 }
 
-func CreateToken(userName string, password string) string {
-
-	hmacSampleSecret := []byte("my_secret_key")
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"username": userName,
-		"password": password,
-		"nbf":      time.Date(2019, 10, 10, 12, 0, 0, 0, time.UTC).Unix(),
-	})
-	tokenString, err := token.SignedString(hmacSampleSecret)
-	fmt.Println(tokenString, err)
-	fmt.Println("===name====")
-	fmt.Println(VerifyToken(tokenString))
-	return tokenString
-
-}
-
-func VerifyToken(tokenString string) (bool, string) {
-	var name string
-	hmacSampleSecret := []byte("my_secret_key")
-	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-		// Don't forget to validate the alg is what you expect:
-		//if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-		//	return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
-		//}
-
-		// hmacSampleSecret is a []byte containing your secret, e.g. []byte("my_secret_key")
-		return hmacSampleSecret, nil
-	})
-
+func authHandler(c *gin.Context) {
+	// 用户发送用户名和密码过来
+	var user User
+	err := c.ShouldBind(&user)
 	if err != nil {
-		log.Print("请检查 token 参数！")
-		return false, name
+		c.JSON(http.StatusOK, gin.H{
+			"code": 2001,
+			"msg":  "无效的参数",
+		})
+		return
 	}
-
-	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
-		fmt.Println(claims["username"], claims["nbf"])
-		name = claims["username"].(string)
-	} else {
-		fmt.Println(err)
-		return false, name
+	// 校验用户名和密码是否正确
+	if user.UserName == "admin" && user.Password == "pass" {
+		// 生成Token
+		tokenString, _ := GenToken(user.UserName)
+		c.JSON(http.StatusOK, gin.H{
+			"code": 2000,
+			"msg":  "success",
+			"data": gin.H{"token": tokenString},
+		})
+		return
 	}
+	c.JSON(http.StatusOK, gin.H{
+		"code": 2002,
+		"msg":  "鉴权失败",
+	})
+	return
+}
 
-	return true, name
+// GenToken 生成JWT
+func GenToken(username string) (string, error) {
+	// 创建一个我们自己的声明
+	c := MyClaims{
+		username, // 自定义字段
+		jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(TokenExpireDuration).Unix(), // 过期时间
+			Issuer:    "my-project",                               // 签发人
+		},
+	}
+	// 使用指定的签名方法创建签名对象
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, c)
+	// 使用指定的secret签名并获得完整的编码后的字符串token
+	return token.SignedString(MySecret)
+}
+
+// ParseToken 解析JWT
+func ParseToken(tokenString string) (*MyClaims, error) {
+	// 解析token
+	token, err := jwt.ParseWithClaims(tokenString, &MyClaims{}, func(token *jwt.Token) (i interface{}, err error) {
+		return MySecret, nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	if claims, ok := token.Claims.(*MyClaims); ok && token.Valid { // 校验token
+		return claims, nil
+	}
+	return nil, errors.New("invalid token")
+}
+
+// JWTAuthMiddleware 基于JWT的认证中间件
+func JWTAuthMiddleware() func(c *gin.Context) {
+	return func(c *gin.Context) {
+		// 客户端携带Token有三种方式 1.放在请求头 2.放在请求体 3.放在URI
+		// 这里假设Token放在Header的Authorization中，并使用Bearer开头
+		// 这里的具体实现方式要依据你的实际业务情况决定
+		authHeader := c.Request.Header.Get("Authorization")
+		if authHeader == "" {
+			c.JSON(http.StatusOK, gin.H{
+				"code": 2003,
+				"msg":  "请求头中auth为空",
+			})
+			c.Abort()
+			return
+		}
+		// 按空格分割
+		parts := strings.SplitN(authHeader, " ", 2)
+		if !(len(parts) == 2 && parts[0] == "Bearer") {
+			c.JSON(http.StatusOK, gin.H{
+				"code": 2004,
+				"msg":  "请求头中auth格式有误",
+			})
+			c.Abort()
+			return
+		}
+		// parts[1]是获取到的tokenString，我们使用之前定义好的解析JWT的函数来解析它
+		mc, err := ParseToken(parts[1])
+		if err != nil {
+			c.JSON(http.StatusOK, gin.H{
+				"code": 2005,
+				"msg":  "无效的Token",
+			})
+			c.Abort()
+			return
+		}
+		// 将当前请求的username信息保存到请求的上下文c上
+		c.Set("username", mc.Username)
+		c.Next() // 后续的处理函数可以用过c.Get("username")来获取当前请求的用户信息
+	}
 }
