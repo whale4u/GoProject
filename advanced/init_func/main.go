fecf9c2bebbe90d61403a7061b148ddb958aef06
whale4u <whale4u@hotmail.com>
2021-05-18 12:48:18 +0800

    移动部分代码

---
+++ advanced/init_func/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func init() {
+	fmt.Println("init1:", a)
+}
+
+func init() {
+	fmt.Println("init2:", a)
+}
+
+var a = 10
+
+const b = 100
+
+func main() {
+	fmt.Println("main:", a)
+}
+
+//init1: 10
+//init2: 10
+//main: 10
