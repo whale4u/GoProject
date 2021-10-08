8ce63cb0b4ca74bfa93bd0146a73ad10ea35fce5
whale4u <whale4u@hotmail.com>
2021-10-08 22:50:36 +0800

    https://github.com/itsmikej/imooc_logprocess/

---
+++ project/imooc_v3/log_process.go
@@ -0,0 +1,154 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"net/url"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+	"time"
+)
+
+type Reader interface {
+	Read(rc chan []byte)
+}
+
+type Writer interface {
+	Write(wc chan *Message)
+}
+
+type LogProcess struct {
+	rc    chan []byte
+	wc    chan *Message
+	read  Reader
+	write Writer
+}
+
+type ReadFromFile struct {
+	path string // 读取文件路径
+}
+
+type Message struct {
+	TimeLocal                    time.Time
+	BytesSent                    int
+	Path, Method, Scheme, Status string
+	UpstreamTime, RequestTime    float64
+}
+
+type WriteToInfluxDB struct {
+	influxDBDsn string // influx data source
+}
+
+func (r *ReadFromFile) Read(rc chan []byte) {
+	//	读取模块
+	//line := "message"
+	//rc <- line
+	// 打开文件
+	f, err := os.Open(r.path)
+	if err != nil {
+		panic(fmt.Sprintf("open file error:%s", err.Error()))
+	}
+	// 从文件末尾开始读取文件内容
+	f.Seek(0, 2)
+	rd := bufio.NewReader(f)
+
+	for {
+		line, err := rd.ReadBytes('\n')
+		if err == io.EOF {
+			time.Sleep(500 * time.Millisecond)
+			continue
+		} else if err != nil {
+			panic(fmt.Sprintf("ReadBytes error:%s", err.Error()))
+		}
+		//去掉换行符
+		rc <- line[:len(line)-1]
+	}
+
+}
+
+func (w *WriteToInfluxDB) Write(wc chan *Message) {
+	//	写入模块
+	//fmt.Println(<-wc)
+	for v := range wc {
+		fmt.Println(v)
+	}
+}
+
+func (l *LogProcess) Process() {
+	//	解析模块
+	//data := <-l.rc
+	//l.wc <- strings.ToUpper(string(data))
+
+	r := regexp.MustCompile(`([\d\.]+)\s+([^ \[]+)\s+([^ \[]+)\s+\[([^\]]+)\]\s+([a-z]+)\s+\"([^"]+)\"\s+(\d{3})\s+(\d+)\s+\"([^"]+)\"\s+\"(.*?)\"\s+\"([\d\.-]+)\"\s+([\d\.-]+)\s+([\d\.-]+)`)
+	loc, _ := time.LoadLocation("Asia/Shanghai")
+	for v := range l.rc {
+		ret := r.FindStringSubmatch(string(v))
+		//14个括号，长度等于14说明匹配成功
+		if len(ret) != 14 {
+			log.Println("FindStringSubmatch fail：", string(v))
+			continue
+		}
+
+		message := &Message{}
+		t, err := time.ParseInLocation("02/Jan/2006:15:04:05 +0000", ret[4], loc)
+		if err != nil {
+			log.Println("ParseInLocation fail:", err.Error(), ret[4])
+		}
+		message.TimeLocal = t
+
+		byteSent, _ := strconv.Atoi(ret[8])
+		message.BytesSent = byteSent
+
+		// GET /foo?query=t HTTP/1.0
+		reqSli := strings.Split(ret[6], " ")
+		if len(reqSli) != 3 {
+			log.Println("string.Split fail", ret[6])
+			continue
+		}
+		message.Method = reqSli[0]
+
+		u, err := url.Parse(reqSli[1])
+		if err != nil {
+			log.Println("url parse fail:", err)
+			continue
+		}
+		message.Path = u.Path
+
+		message.Scheme = ret[5]
+		message.Status = ret[7]
+
+		upstreamTime, _ := strconv.ParseFloat(ret[12], 64)
+		requestTime, _ := strconv.ParseFloat(ret[13], 64)
+		message.UpstreamTime = upstreamTime
+		message.RequestTime = requestTime
+
+		l.wc <- message
+		//l.wc <- strings.ToUpper(string(v))
+	}
+}
+
+func main() {
+	r := &ReadFromFile{
+		path: "/Users/whale4u/Code/GoProject/project/access.log",
+	}
+
+	w := &WriteToInfluxDB{
+		influxDBDsn: "username&password..",
+	}
+
+	lp := &LogProcess{
+		rc:    make(chan []byte),
+		wc:    make(chan *Message),
+		read:  r,
+		write: w,
+	}
+	go lp.read.Read(lp.rc)
+	go lp.Process()
+	go lp.write.Write(lp.wc)
+
+	time.Sleep(30 * time.Second) //等待协程执行完成
+}
